e00443f821fe07c67c999541f98ac7cb0a8545b5
Junior Prado <juniorcvnnn@gmail.com>
2025-02-21 17:17:36 -0300

    fix dataabase

--- backend/user-service/db/init.sql
+++ backend/user-service/db/init.sql
@@ -1,7 +1,8 @@
 -- Active: 1740117052385@@127.0.0.1@5435@user_db
 CREATE TABLE "user" (
     id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     role VARCHAR(50) NOT NULL CHECK (role IN ('user', 'admin'))
 );
