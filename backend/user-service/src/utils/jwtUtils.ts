2b65e927264ab7f0b70caca3271a139957455788
Junior Prado <juniorcvnnn@gmail.com>
2025-02-21 17:18:14 -0300

    add utils

---
+++ backend/user-service/src/utils/jwtUtils.ts
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+import { UserPayload } from '../models/userModel';
+
+// Chave secreta para assinar e verificar tokens
+const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key';
+
+// Gera um token JWT
+export const generateToken = (payload: UserPayload): string => {
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' }); // Token expira em 1 hora
+};
+
+// Verifica e extrai os dados do token
+export const verifyToken = (token: string): UserPayload => {
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as UserPayload;
+    return decoded;
+  } catch (err) {
+    throw new Error('Invalid token');
+  }
+};
