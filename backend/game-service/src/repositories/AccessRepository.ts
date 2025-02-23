fc33b13132d8b94acf4fda8dd95fb571ad8770a9
JuniorPrado-dev <juniorcvnnn@gmail.com>
2025-02-23 20:05:07 -0300

    fix: fix db

--- backend/game-service/src/repositories/AccessRepository.ts
+++ backend/game-service/src/repositories/AccessRepository.ts
@@ -34,7 +34,7 @@ export class AccessRepository {
                                     email,
                                     created_at,
                                     id_post,
-                                    status,
+                                    status
                                     ) 
                  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,
                 [id, utm_campaign, utm_channel, utm_medium, utm_source,email,created_at,id_post,status]
